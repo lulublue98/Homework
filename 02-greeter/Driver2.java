55aa18dde4ebfdaad9068dfa24a13bc3394f3381
lulublue98 <lulu.jiyun.cho@gmail.com>
2014-09-17 08:23:59 -0400

    "New Greeter / Driver"

---
+++ 02-greeter/Driver2.java
@@ -0,0 +1,10 @@
+public class Driver2 {
+    public static void main(String[] args) {
+	Greeter2 g1 = new Greeter2();
+	Greeter2 g2 = new Greeter2();
+	g1.greet2();
+	g2.greet2();
+
+    }
+
+}
---
+++ 02-greeter/Greeter2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Greeter2 {
+    public void greet2() {
+	String greeting = new String("Sup!");
+	System.out.println(greeting);
+    }
+    public void ungreet2() {
+	System.out.println("I'm out!");
+    }
+
+}
