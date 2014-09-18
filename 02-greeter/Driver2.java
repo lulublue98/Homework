8ae89f378746737cfd3ae7289ea60da1c257acdf
lulublue98 <lulu.jiyun.cho@gmail.com>
2014-09-18 08:14:05 -0400

    New Driver

--- 02-greeter/Driver2.java
+++ 02-greeter/Driver2.java
@@ -2,8 +2,20 @@ public class Driver2 {
     public static void main(String[] args) {
 	Greeter2 g1 = new Greeter2();
 	Greeter2 g2 = new Greeter2();
-	g1.greet2();
-	g2.greet2();
+	g1.greet();
+	g2.greet();
+
+	// "get" and "set are accessor methods
+	// because instance variables should be private, we need to do this:
+	g1.setGreeting( new String("Sup!"));
+
+	g1.greet();
+	g2.greet();
+	
+	g2.setGreeting("Hello!");
+	
+	g1.greet();
+	g2.greet();
 
     }
 
