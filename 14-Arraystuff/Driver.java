c0123ffd731f146fefa46382bb345a1c9cad0e5f
Lulu Cho <lulu.jiyun.cho@gmail.com>
2014-10-28 08:40:13 -0400

    Lulu

--- 14-Arraystuff/Driver.java
+++ 14-Arraystuff/Driver.java
@@ -3,8 +3,10 @@ public class Driver {
 	Arrays A1 = new Arrays();
 	System.out.println(A1);
 	int x = A1.find(120);
-	System.out.println(x);
+	System.out.println("120 is at index " + x);
 	int y = A1.maxVal();
-	System.out.println(y);
+	System.out.println("the maximum value is " + y);
+	int z = A1.freq(112);
+	System.out.println("112 occurs " + z + " times");
     }
 }
