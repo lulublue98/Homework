177dbc6c2ac3b70c0d35477499bdd88026e6d2d6
lulublue98 <lulu.jiyun.cho@gmail.com>
2014-10-27 22:03:42 -0400

    Arrays

--- 14-Arraystuff/Arrays.java
+++ 14-Arraystuff/Arrays.java
@@ -1,3 +1,49 @@
+import java.util.*;
+
 public class Arrays {
+
+    private int[] a;
+    private Random rnd;
+    public final int final_example = 123;
     
+    public Arrays(int n){
+	rnd = new Random();
+	a = new int[n];
+	for (int i=0;i<a.length;i++){
+	    a[i] = 75+rnd.nextInt(76);
+	}
+    }
+    
+    public Arrays() {
+	this(100);
+    }
+
+    public String toString(){
+	String s="";
+	for (int i=0;i<a.length;i++){
+	    s=s+a[i]+", ";
+	}
+	return s;
+    }
+
+    public int find(int n) {
+	int x;
+	for (x=0;x<a.length;x=x+1) {
+	    if (n == a[x]) {
+		return x;
+	    }
+	}
+	return -1;
+    }
+
+    public int maxVal() {
+	int x;
+	int y = 0;
+	for (x=0;x<a.length;x=x+1) {
+	    if ( y < a[x] ) {
+		y = a[x];
+	    }
+	}
+	return y;
+    }
 }
---
+++ 14-Arraystuff/Driver.java
@@ -0,0 +1,10 @@
+public class Driver {
+    public static void main(String[] args) {
+	Arrays A1 = new Arrays();
+	System.out.println(A1);
+	int x = A1.find(120);
+	System.out.println(x);
+	int y = A1.maxVal();
+	System.out.println(y);
+    }
+}
