9668bd82779e32570802b3b8252824e952ce6ebf
Lulu Cho <lulu.jiyun.cho@gmail.com>
2014-11-12 08:42:12 -0500

    lulublue

--- 15-SuperArray/DriverL.java
+++ 15-SuperArray/DriverL.java
@@ -1,5 +1,16 @@
+import java.io.*;
+import java.util.*;
+
 public class DriverL {
     public static void main( String[] args ) {
+
+	ArrayList<Integer> ai;
+	ai=new ArrayList<Integer>();
+	Random r = new Random();
+	for (int i = 0; i < 20; i++) {
+	    ai.add(r.nextInt(5));
+	}
+	System.out.println(ai);
 
     }
 }
