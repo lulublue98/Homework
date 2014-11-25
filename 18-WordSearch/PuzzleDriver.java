58af40cd7fe8192ea2f7e4406ddaf56731ed23b6
Lulu Cho <lulu.jiyun.cho@gmail.com>
2014-11-25 08:37:19 -0500

    bad to the bone

--- 18-WordSearch/PuzzleDriver.java
+++ 18-WordSearch/PuzzleDriver.java
@@ -1,10 +1,11 @@
 public class PuzzleDriver {
 
     public static void main(String[] args) {
-	WordSearch W = new WordSearch(30,50,"LameVillians.txt",24);
-	W.MakePuzzle();
-	System.out.println();
-	System.out.println(W.PrintPuzzle());
+	WordSearch W = new WordSearch(30,50,"LameVillains.txt",24);
+	W.ReadFile();
+	W.wordAdder();
+	System.out.println(W);
+	//System.out.println(W.PrintPuzzle());
 
     }
 
