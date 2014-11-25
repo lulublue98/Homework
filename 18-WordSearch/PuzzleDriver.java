b4837dc6b0e358453e5e462c7685cbcb89ed5825
lulublue98 <lulu.jiyun.cho@gmail.com>
2014-11-24 22:33:53 -0500

    I'm a Stuy kid, not a Computer Scientist!

--- 18-WordSearch/PuzzleDriver.java
+++ 18-WordSearch/PuzzleDriver.java
@@ -1,7 +1,7 @@
 public class PuzzleDriver {
 
     public static void main(String[] args) {
-	WordSearch W = new WordSearch(30,50,"LameVillians.txt",20);
+	WordSearch W = new WordSearch(30,50,"LameVillians.txt",24);
 	W.MakePuzzle();
 	System.out.println();
 	System.out.println(W.PrintPuzzle());
