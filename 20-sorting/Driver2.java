f227369458b7eaf6d2f636ea93a2dcf65fa054b3
Lulu Cho <lulu.jiyun.cho@gmail.com>
2014-12-09 08:37:05 -0500

    sorting hat

---
+++ 20-sorting/Driver2.java
@@ -0,0 +1,11 @@
+public class Driver2 {
+    
+    public static void main(String[] args) {
+	InterArray Intervals = new InterArray(10);
+	System.out.println(Intervals);
+	Intervals.sort();
+	System.out.println(Intervals);
+	
+    }
+
+}
---
+++ 20-sorting/InterArray.java
@@ -0,0 +1,24 @@
+public class InterArray {
+    
+    private Interval[] Intervals;
+
+    public InterArray(int len) {
+	Intervals = new Interval[len];
+	for (int i=0;i<len;i++) {
+	    Intervals[i] = new Interval();
+	}
+    }
+
+    public String toString() {
+	String end = "";
+	for (int i=0;i<Intervals.length;i++) {
+	    end = end + Intervals[i] + "  ";
+	}
+	return end;
+    }
+
+    public void sort() {
+	
+    }
+
+}
