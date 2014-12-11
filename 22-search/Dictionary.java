97920212f0d55ccaaf140cf30f205f843a4fa38d
Lulu Cho <lulu.jiyun.cho@gmail.com>
2014-12-11 08:41:56 -0500

    oh boy

--- 22-search/Dictionary.java
+++ 22-search/Dictionary.java
@@ -8,4 +8,16 @@ public class Dictionary {
 	len = length - 1;
     }
 
+    public void add(object thing) {
+	int index = 0;
+	for (int x=0;Dictionary[x]==null;x=x+1) {
+	    index = index + 1;
+	}
+	Dictionary[index] = thing;
+    }
+
+    public Comparable search() {
+
+    }
+
 }
