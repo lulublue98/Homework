05424ae8482bf11979886f547e747acc7da760eb
lulublue98 <lulu.jiyun.cho@gmail.com>
2014-12-10 22:36:36 -0500

    testing...

---
+++ 21-sorting/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+
+    public static void main(String[] args) {
+	
+	Warrior[] Warriors = new Warrior[10];
+	Arrays.sort(Warriors);
+	System.out.println(Warriors);
+
+    }
+
+}
