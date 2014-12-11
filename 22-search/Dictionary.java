beabf3623ada45c1745c28680be125ec7a86af21
Lulu Cho <lulu.jiyun.cho@gmail.com>
2014-12-11 08:38:26 -0500

    Comparable?

--- 22-search/Dictionary.java
+++ 22-search/Dictionary.java
@@ -1,3 +1,11 @@
 public class Dictionary {
 
+    private Comparable[] Dictionary;
+    private int len;
+
+    public Dictionary(int length) {
+	Dictionary = new Comparable[length];
+	len = length - 1;
+    }
+
 }
