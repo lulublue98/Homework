42793f7746d7f4e6b3347d3e078f2a6a56371b49
lulublue98 <lulu.jiyun.cho@gmail.com>
2014-12-14 23:54:13 -0500

    I'm Batman

--- 22-search/Driver.java
+++ 22-search/Driver.java
@@ -1,7 +1,11 @@
 public class Driver {
 
     public static void main(String[] args) {
-
+	Searching A = new Searching(10);
+	for (int x=0;x<10;x++) {
+	    A.add(x+1);
+	}
+	System.out.println(A);
     }
 
 }
--- 22-search/Searching.java
+++ 22-search/Searching.java
@@ -5,19 +5,41 @@ public class Searching {
 
     public Searching(int length) {
 	Dictionary = new Comparable[length];
-	len = length - 1;
+	for (int x=0;x<len;x++) {
+	    Dictionary[x] = null;
+	}
+	len = length;
     }
 
-    public void add(object thing) {
+    public void add(Comparable thing) {
 	int index = 0;
 	for (int x=0;Dictionary[x]==null;x=x+1) {
 	    index = index + 1;
 	}
 	Dictionary[index] = thing;
     }
-
-    public Comparable search() {
-
+    
+    public void sort() {
+	Arrays.sort(Dictionary);
+    }
+    
+    public Comparable lsearch(Comparable thing) {
+	for (int x=0;x<len;x++) {
+	    if (Dictionary[x]==thing) {
+		return Dictionary[x];
+	    }
+	}
+	return null;
     }
 
+    public Comparable bsearch(Comparable thing) {
+	Comparable temp;
+	int index = len;
+	while (thing!=temp) {
+	    temp = Dictionary[len];
+	    index = index / 2;
+	}
+	return temp;
+    }
+    
 }
